31ef1732a32be984a446453ce576bb87d145977a
lodubot <jaihacku@gmail.com>
2026-07-30 17:47:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@
 
 #ESI CHANGE MAT KARNA BHAI PLZ RESPECT ALL DEVELOPER 
 # Your Telegram Bot Token (from @BotFather)
-BOT_TOKEN = "8663644709:AAGSCfNrs7AN_oDHApKYaAFZi0iLFdyoygM"
+BOT_TOKEN = "8939341047:AAEwTTXMeKF3hEgkCpk5wZV8E2vgArCmKx0"
 
 # Admin User IDs (list of Telegram user IDs)
 ADMIN_IDS = [
